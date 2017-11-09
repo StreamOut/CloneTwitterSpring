0c77d879ff38dc526eaaaa508b5851dfa5b41c1c
DUMONT Malo <d12007201@ESJFST-S322L03.salsa.univ-amu.fr>
2017-11-09 10:32:33 +0100

    initial commit

---
+++ src/main/java/amu/m2sir/malodumont/CloneTwitterSpring/CloneTwitterSpringApplication.java
@@ -0,0 +1,12 @@
+package amu.m2sir.malodumont.CloneTwitterSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneTwitterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneTwitterSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/amu/m2sir/malodumont/CloneTwitterSpring/CloneTwitterSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package amu.m2sir.malodumont.CloneTwitterSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloneTwitterSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
