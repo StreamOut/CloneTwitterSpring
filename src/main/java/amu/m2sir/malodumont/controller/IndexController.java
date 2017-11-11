babd91f2c8705cae3e5b559483ff31de188b50a9
Malo <malodumont@gmail.com>
2017-11-11 14:07:44 +0100

    correction pom

--- src/main/java/amu/m2sir/malodumont/controller/IndexController.java
+++ src/main/java/amu/m2sir/malodumont/controller/IndexController.java
@@ -1,12 +1,16 @@
 package amu.m2sir.malodumont.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
-	@GetMapping("/")
-	public String index() {
-		return "index"; // equivalent du forward
-	}
+
+	 @RequestMapping("/index")
+    public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+	 }
 }
